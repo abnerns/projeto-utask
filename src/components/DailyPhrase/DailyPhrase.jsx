0f644b2a4f99d1d80e0498054ae76833f3a51a94
Abner do Nascimento <abnerdonantos06@gmail.com>
2024-11-13 21:17:14 -0300

    feat: update daily phrase api translation

--- src/components/DailyPhrase/DailyPhrase.jsx
+++ src/components/DailyPhrase/DailyPhrase.jsx
@@ -1,8 +1,32 @@
 import { MdTipsAndUpdates } from 'react-icons/md'
 import styles from './DailyPhrase.module.css'
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react';
 
 const DailyPhrase = ({ darkMode, responsive }) => {
+  const [phrase, setPhrase] = useState('');
+
+  useEffect(() => {
+    const fetchPhrase = async () => {
+      try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        const englishPhrase = data.slip.advice;
+
+        const translate = await fetch(
+          `https://api.mymemory.translated.net/get?q=${encodeURIComponent(englishPhrase)}&langpair=en|pt-BR`
+        );
+        const translateData = await translate.json();
+        const translatePhrase = translateData.responseData.translatedText;
+
+        setPhrase(translatePhrase);
+      } catch (error) {
+        console.error('Erro ao buscar ou traduzir a frase:', error);
+      }
+    };
+
+    fetchPhrase();
+  }, []);
 
   return responsive ? (
     <div style={{display: "flex", flexDirection: "column", gap: "1rem"}} className={darkMode ? styles.dark : ''}>
@@ -10,14 +34,14 @@ const DailyPhrase = ({ darkMode, responsive }) => {
         <div className={styles.icon}><MdTipsAndUpdates size={30} /></div>
         <p style={{fontWeight: "bold"}}>Frase do dia</p>
       </div>
-      <p>Se você quer um pedacinho do paraíso, acredite em Deus. Mas se você quer conquistar o mundo, acredite em você porque Deus já te deu tudo o que você precisa para você vencer.</p>
+      <p>{phrase || 'Carregando...'}</p>
     </div>
   ) : (
     <div className={`${styles.container} ${darkMode ? styles.dark : ''}`}>
       <div className={styles.icon}><MdTipsAndUpdates size={30} /></div>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
         <p style={{ fontSize: '12px', fontWeight: 'bold' }}>Frase do dia</p>
-        <p>Se você quer um pedacinho do paraíso, acredite em Deus. Mas se você quer conquistar o mundo, acredite em você porque Deus já te deu tudo o que você precisa para você vencer.</p>
+        <p>{phrase || 'Carregando...'}</p>
       </div>
     </div>
   );
